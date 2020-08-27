0a6e485c5caf9236a66ea78ba689e6b497c17905
Deekshanta-818 <deekshantapaudel@gmail.com>
2020-08-27 16:01:23 +0545

    My commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,51 @@
-import React,{useState} from 'react';
-import './App.css'
-import News from './News.jsx'
-const App = () => {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import News from './News'
+import './App.css';
+ 
+function App() {
+  const [news, setNews] = useState([]);
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://newsapi.org/v2/top-headlines?country=us&apiKey=7271e80c537c43f18621ebc193be7ca5',
+      );
+ 
+      setNews(result.data.articles);
+      console.log(result.data.articles)
+    };
+ 
+    fetchData();
+  }, []);
 
-    const [news,setNews] = useState([])
 
-   
+  return (
+    <div className='container'>
+    <section className='container mb-5'>
+        <h1 className='text-center xs heading'>The Latest News Web Application</h1>
+        
+       
 
-const getNews = async () => {
-    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=7271e80c537c43f18621ebc193be7ca5`)
-    const data= await response.json();
-    setNews(data.articles)
-    console.log(news)
-    
- }
+    </section>
 
+    <div className='News'>
+    {news.map(info => (
+                   <News
+                   key={info.title} 
+                   author={info.author}
+                   title={info.title}
+                   description={info.description}
+                   content={info.content}
+                   urlToImage={info.urlToImage}
+                   url={info.url}
+                   publishedAt={info.publishedAt}
+                   />
+    ))}
+    </div>
+</div>
+  )
 
-    return (
-        <div className='container'>
-            <section className='container mb-5'>
-                <h1 className='text-center xs heading'>The Latest News Web Application</h1>
-                
-                <button type="button" className="btn btn-outline-primary btn-lg " style={{marginLeft:'450px',marginTop:'10px'}} onClick={getNews}>Get The Latest News</button>
-
-            </section>
-
-            <div className='News'>
-            {news.map(info => (
-                           <News
-                           key={info.author} 
-                           author={info.author}
-                           title={info.title}
-                           description={info.description}
-                           content={info.content}
-                           urlToImage={info.urlToImage}
-                           url={info.url}
-                           publishedAt={info.publishedAt}
-                           />
-            ))}
-            </div>
-        </div>
-    )
 }
-
-export default App
+ 
+export default App;
